5205aba105bda1a051f596b50ce71dd57b92883c
Thiago <programadorthiagolopes@gmail.com>
2023-03-22 17:04:19 -0300

    feat: AbstractODM

---
+++ src/Models/AbstractODM.ts
@@ -0,0 +1,25 @@
+import { Model, Schema, model, models } from 'mongoose';
+
+abstract class AbstractODM<T> {
+  readonly model: Model<T>;
+  private schema: Schema<T>;
+
+  constructor(schema: Schema<T>, modelName: string) {
+    this.schema = schema;
+    this.model = models[modelName] || model(modelName, this.schema);
+  }
+
+  public async create(obj: T): Promise<T> {
+    return this.model.create(obj);
+  }
+
+  public async findById(id: string): Promise<T | null> {
+    return this.model.findById(id);
+  }
+
+  public async findAll(): Promise<T[]> {
+    return this.model.find();
+  }
+}
+
+export default AbstractODM;
