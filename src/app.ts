e0bea175c07613d22d38bd57a4aa3006789db022
Thiago <programadorthiagolopes@gmail.com>
2023-03-22 18:28:34 -0300

    feat: Add ErrorHandler

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import carsRouter from './Routes/CarRoutes';
+import ErrorHandler from './Middlewares/ErrorHandler';
 
 const app = express();
 app.use(express.json());
 app.use('/cars', carsRouter);
+app.use(ErrorHandler.handle);
 
 export default app;
