3f6b6141aca4a2e4554a85799c6b015a071123e9
Thiago <programadorthiagolopes@gmail.com>
2023-03-22 17:03:46 -0300

    feat: Criado rota para cars

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import carsRouter from './Routes/CarRoutes';
 
 const app = express();
+app.use(express.json());
+app.use('/cars', carsRouter);
 
 export default app;
