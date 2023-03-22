6fc23f2c4cda483c4c667ee41913b7232aea69d8
Thiago <programadorthiagolopes@gmail.com>
2023-03-22 20:21:13 -0300

    feat: Criado rota motorcycles

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import carsRouter from './Routes/CarRoutes';
 import ErrorHandler from './Middlewares/ErrorHandler';
+import motocycleRouter from './Routes/MotocycleRoutes';
 
 const app = express();
 app.use(express.json());
+app.use('/motorcycles', motocycleRouter);
 app.use('/cars', carsRouter);
 app.use(ErrorHandler.handle);
 
