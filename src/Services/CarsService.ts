bd46aeeec289ffe35f35eeb30d296c3a336aff7b
Thiago <programadorthiagolopes@gmail.com>
2023-03-22 18:29:26 -0300

    feat: Car Get`s

--- src/Services/CarsService.ts
+++ src/Services/CarsService.ts
@@ -1,6 +1,8 @@
+import { isValidObjectId } from 'mongoose';
 import Car from '../Domains/Car';
 import ICar from '../Interfaces/ICar';
 import CarsODM from '../Models/CarODM';
+import ErrorTeste from '../utils/ClassError';
 
 class CarsService {
   private _modelODM: CarsODM;
@@ -20,6 +22,29 @@ class CarsService {
   public async createCar(objCar: ICar): Promise<ICar | null> {
     const created = await this._modelODM.create(objCar);
     return this.createCarDomain(created);
+  }
+
+  public async findCars(id: string): Promise<ICar | null> {
+    if (!isValidObjectId(id)) throw new ErrorTeste('Invalid mongo id', 422);
+    const cars = await this._modelODM.findById(id);
+    if (cars === null) throw new ErrorTeste('Car not found', 404);
+    return this.createCarDomain(cars);
+  }
+
+  public async findAllCars(): Promise<ICar[] | null> {
+    const cars = await this._modelODM.findAll();
+    const result = cars.map((car) => ({
+      id: car.id,
+      model: car.model,
+      year: car.year,
+      color: car.color,
+      status: car.status,
+      buyValue: car.buyValue,
+      doorsQty: car.doorsQty,
+      seatsQty: car.seatsQty,
+    }));
+
+    return result;
   }
 }
 
