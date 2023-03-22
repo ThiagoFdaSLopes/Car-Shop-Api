f649cc8356e2ad6ada034ed049774d4e3eb0e217
Thiago <programadorthiagolopes@gmail.com>
2023-03-22 18:28:44 -0300

    feat: FindCars

--- src/Controllers/CarController.ts
+++ src/Controllers/CarController.ts
@@ -19,6 +19,32 @@ class CarController {
       next(error);
     }
   }
+
+  public async findAllCars(
+    _req: Request,
+    res: Response, 
+    next: NextFunction,
+  ): Promise<Response | undefined> {
+    try {
+      const carCreated = await this.carsService.findAllCars();
+      return res.status(200).json(carCreated);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  public async findCarById(
+    req: Request,
+    res: Response, 
+    next: NextFunction,
+  ): Promise<Response | undefined> {
+    try {
+      const carCreated = await this.carsService.findCars(req.params.id);
+      return res.status(200).json(carCreated);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 export default CarController;
