6dfe16b76d3c8b0125be59b7183f62f381df2228
Thiago <programadorthiagolopes@gmail.com>
2023-03-22 20:22:24 -0300

    feat: Motorcycle ODM

---
+++ src/Models/MotorcycleODM.ts
@@ -0,0 +1,21 @@
+import { Schema } from 'mongoose';
+import AbstractODM from './AbstractODM';
+import IMotorcycle from '../Interfaces/IMotorcycle';
+
+class MotorcycleODM extends AbstractODM<IMotorcycle> {
+  constructor() {
+    const schema = new Schema<IMotorcycle>({
+      model: { type: String, required: true },
+      year: { type: Number, required: true },
+      color: { type: String, required: true },
+      buyValue: { type: Number, required: true },
+      category: { type: String, required: true },
+      engineCapacity: { type: Number, required: true },
+      status: { type: Boolean, required: false },
+    }, { versionKey: false });
+
+    super(schema, 'Motorcycle');
+  }
+}
+
+export default MotorcycleODM;
