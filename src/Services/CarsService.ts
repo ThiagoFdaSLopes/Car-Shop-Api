689b4c157ad0bb8cf9fc1133f2c0254825711db5
Thiago <programadorthiagolopes@gmail.com>
2023-03-22 22:41:40 -0300

    feat: Criado Rota Service para deletar car

--- src/Services/CarsService.ts
+++ src/Services/CarsService.ts
@@ -6,6 +6,8 @@ import ErrorTeste from '../utils/ClassError';
 
 class CarsService {
   private _modelODM: CarsODM;
+  readonly invalidMongoId: string = 'Invalid mongo id';
+  readonly carNotFound: string = 'Car not found';
 
   constructor(modelODM: CarsODM) {
     this._modelODM = modelODM;
@@ -17,9 +19,9 @@ class CarsService {
   }
 
   public async findCars(id: string): Promise<ICar | null> {
-    if (!isValidObjectId(id)) throw new ErrorTeste('Invalid mongo id', 422);
+    if (!isValidObjectId(id)) throw new ErrorTeste(this.invalidMongoId, 422);
     const cars = await this._modelODM.findById(id);
-    if (cars === null) throw new ErrorTeste('Car not found', 404);
+    if (cars === null) throw new ErrorTeste(this.carNotFound, 404);
     return new Car(cars).CarModel();
   }
 
@@ -40,10 +42,17 @@ class CarsService {
   }
 
   public async UpdateCar(obj: ICar, id: string): Promise<ICar | null> {
-    if (!isValidObjectId(id)) throw new ErrorTeste('Invalid mongo id', 422);
+    if (!isValidObjectId(id)) throw new ErrorTeste(this.invalidMongoId, 422);
     const result = await this._modelODM.updateVehicle(id, obj);
-    if (!result) throw new ErrorTeste('Car not found', 404);
+    if (result === null) throw new ErrorTeste(this.carNotFound, 404);
     return new Car(result).CarModel();
+  }
+
+  public async DeleteCar(id: string): Promise<ICar | null> {
+    if (!isValidObjectId(id)) throw new ErrorTeste(this.invalidMongoId, 422);
+    const result = await this._modelODM.deleteVehicle(id);
+    if (result === null) throw new ErrorTeste(this.carNotFound, 404);
+    return result;
   }
 }
 
