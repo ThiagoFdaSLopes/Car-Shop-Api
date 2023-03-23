e9e14097aa14d4f8a0934d194b5af4f936251367
Thiago <programadorthiagolopes@gmail.com>
2023-03-22 21:25:18 -0300

    feat: Service update, get

--- src/Services/MotorcycleService.ts
+++ src/Services/MotorcycleService.ts
@@ -1,8 +1,8 @@
-// import { isValidObjectId } from 'mongoose';
+import { isValidObjectId } from 'mongoose';
 import Motorcycle from '../Domains/Motorcycle';
 import IMotorcycle from '../Interfaces/IMotorcycle';
 import MotorcycleODM from '../Models/MotorcycleODM';
-// import ErrorTeste from '../utils/ClassError';
+import ErrorTeste from '../utils/ClassError';
 
 class MotorcycleService {
   private _modelODM: MotorcycleODM;
@@ -11,17 +11,39 @@ class MotorcycleService {
     this._modelODM = modelODM;
   }
 
-  private createCarDomain(moto: IMotorcycle | null): IMotorcycle | null {
-    if (moto) {
-      const teste = new Motorcycle(moto);
-      return teste.MotorCiclyModel();
-    }
-    return null;
-  }
-
   public async createCar(objMoto: IMotorcycle): Promise<IMotorcycle | null> {
     const created = await this._modelODM.create(objMoto);
-    return this.createCarDomain(created);
+    return new Motorcycle(created).MotorCiclyModel();
+  }
+
+  public async findMotors(id: string): Promise<IMotorcycle | null> {
+    if (!isValidObjectId(id)) throw new ErrorTeste('Invalid mongo id', 422);
+    const motors = await this._modelODM.findById(id);
+    if (motors === null) throw new ErrorTeste('Motorcycle not found', 404);
+    return new Motorcycle(motors).MotorCiclyModel();
+  }
+
+  public async findAllMotors(): Promise<IMotorcycle[] | null> {
+    const motors = await this._modelODM.findAll();
+    const result = motors.map((moto: IMotorcycle) => ({
+      id: moto.id,
+      model: moto.model,
+      year: moto.year,
+      color: moto.color,
+      status: moto.status,
+      buyValue: moto.buyValue,
+      category: moto.category,
+      engineCapacity: moto.engineCapacity,
+    }));
+
+    return result;
+  }
+
+  public async UpdateMoto(obj: IMotorcycle, id: string): Promise<IMotorcycle | null> {
+    if (!isValidObjectId(id)) throw new ErrorTeste('Invalid mongo id', 422);
+    const result = await this._modelODM.updateVehicle(id, obj);
+    if (!result) throw new ErrorTeste('Motorcycle not found', 404);
+    return new Motorcycle(result).MotorCiclyModel();
   }
 }
 
